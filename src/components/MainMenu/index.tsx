d5ccf642a6a1e9eb1598d8f4c96b336d3bb75bd0
Ali Haghighatkhah <ali@lightcurve.io>
2022-06-05 15:36:12 +0200

    Fix icons

--- src/components/MainMenu/index.tsx
+++ src/components/MainMenu/index.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { IconLink } from '../common/Link';
 import UserCollections from '../UserCollections';
+import logo from '../../assets/images/logo.svg';
 import styles from './mainMenu.css';
 
 const MainMenu = () => (
   <aside className={styles.wrapper}>
-    <div>
+    <div className={styles.logo}>
+      <a href="/">
+        <img src={logo} alt="Free Muse" />
+      </a>
+    </div>
+    <div className={styles.list}>
       <IconLink
         icon="home"
         target="home"
+        className={`${styles.menuItem} ${styles.active}`}
+      />
+      <IconLink
+        icon="search"
+        target="search"
+        className={styles.menuItem}
+      />
+      <IconLink
+        icon="user"
+        target="profile"
         className={styles.menuItem}
       />
       <IconLink
         icon="settings"
         target="settings"
-        className={styles.menuItem}
-      />
-      <IconLink
-        icon="profile"
-        target="profile"
         className={styles.menuItem}
       />
     </div>
