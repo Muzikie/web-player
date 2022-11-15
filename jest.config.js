fb06ac81cd586bc012c5c22da093cd8bca09e8b3
Ali Haghighatkhah <ali@lightcurve.io>
2022-11-15 13:11:04 +0100

    Add test dependecies

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
+const { resolve } = require('path');
+
 module.exports = {
   rootDir: './app',
   resetModules: true,
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'jsdom',
+  resetMocks: false,
+  setupFiles: ['jest-localstorage-mock'],
+  moduleNameMapper: {
+    '^~(.*)$': resolve(__dirname, './app/$1'),
+  },
 };
