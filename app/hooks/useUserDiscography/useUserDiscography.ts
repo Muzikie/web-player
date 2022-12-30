f63a668576470bb45163bce4bc7e1cb436cbb293
Ali Haghighatkhah <ali@lightcurve.io>
2022-12-30 16:24:40 +0100

    Create useUserDiscography hook and use it to load collections/albums

---
+++ app/hooks/useUserDiscography/useUserDiscography.ts
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+
+/* Internal dependencies */
+import  { useAccount } from '../useAccount/useAccount';
+import { useWS } from '../useWS/useWS';
+import { AudioAccountResponse, CollectionAccountResponse } from '../useWS/types';
+import { Method } from '../useWS/types';
+import { FEEDBACK_MESSAGES } from './constants';
+
+export const useUserDiscography = () => {
+  const { info: { address } } = useAccount();
+  const { request } = useWS();
+  const [albums, setAlbums] = useState<string[]>([]);
+  const [tracks, setTracks] = useState<string[]>([]);
+
+  const getCollections = async () => {
+    try {
+      const response = <CollectionAccountResponse> await request(Method.collection_getAccount, { address });
+      if (!response.error && response.data.collection.collections.length) {
+        setAlbums(response.data.collection.collections);
+      }
+    } catch (e) {
+      console.log(FEEDBACK_MESSAGES.ERROR_LOADING_ALBUMS, e);
+    }
+  };
+
+  const getTracks = async () => {
+    try {
+      const response = <AudioAccountResponse> await request(Method.audio_getAccount, { address });
+      if (!response.error && response.data.audio.audios.length) {
+        setTracks(response.data.audio.audios);
+      }
+    } catch (e) {
+      console.log(FEEDBACK_MESSAGES.ERROR_LOADING_TRACKS, e);
+    }
+  };
+
+  useEffect(() => {
+    if (address) {
+      getCollections();
+    }
+  }, [address]);
+
+  useEffect(() => {
+    if (albums.length) {
+      getTracks();
+    }
+  }, [albums]);
+
+  return { albums, tracks };
+};
