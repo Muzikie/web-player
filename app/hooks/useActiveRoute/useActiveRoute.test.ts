f99b00d1b56d9242459c4305e052410bf2c01b5e
Sina <sina@noobesang@gmail.com>
2022-12-30 23:08:59 -0500

    impliment useActiveroute test file

---
+++ app/hooks/useActiveRoute/useActiveRoute.test.ts
@@ -0,0 +1,28 @@
+import {renderHook, act} from '@testing-library/react-hooks';
+
+import {useActiveRoute} from './useActiveRoute';
+
+const path = jest.fn(() => ({pathname: 'my-path'}));
+jest.mock('react-router-dom', () => ({
+  useLocation: () => path()
+}));
+
+describe('useActiveRoute', () => {
+  const to = 'my-path';
+
+  it('should return true when path is equal', () => {
+    const {result} = renderHook(() => useActiveRoute(to));
+
+    act(() => {
+      expect(result.current).toBe(true);
+    });
+  });
+  it('should return false when path is not equal', () => {
+    path.mockReturnValueOnce({pathname: 'new'});
+    const {result} = renderHook(() => useActiveRoute(to));
+
+    act(() => {
+      expect(result.current).toBe(false);
+    });
+  });
+});
---
+++ app/hooks/useQueryParams/useQueryParams.test.ts
@@ -0,0 +1,51 @@
+import {renderHook, act} from '@testing-library/react-hooks';
+import {Navigate} from 'react-router-dom';
+
+import {useQueryParams} from './useQueryParams';
+
+jest.mock('react-router-dom', () => ({
+  useLocation: jest.fn(() => ({
+    search: '?foo=bar&baz=qux',
+    pathname: 'http://localhost:3000'
+  })),
+  Navigate: jest.fn()
+}));
+
+describe('useQueryParams', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should convert querystring to an object', () => {
+    const {result} = renderHook(() => useQueryParams());
+
+    expect(result.current.queryParams).toEqual({
+      foo: 'bar',
+      baz: 'qux'
+    });
+  });
+
+  it('should set new key-values to the query string and navigate to the new url', () => {
+    const {result} = renderHook(() => useQueryParams());
+
+    act(() => {
+      result.current.setQueryParam({foo: 'baz', koo: 'liz'});
+    });
+
+    expect(Navigate).toHaveBeenCalledWith({
+      to: 'http://localhost:3000?foo=baz&baz=qux&koo=liz'
+    });
+  });
+
+  it('should remove given key and its value from the query string and navigate to the new url', () => {
+    const {result} = renderHook(() => useQueryParams());
+
+    act(() => {
+      result.current.removeQueryParam('foo');
+    });
+
+    expect(Navigate).toHaveBeenCalledWith({
+      to: 'http://localhost:3000?baz=qux'
+    });
+  });
+});
