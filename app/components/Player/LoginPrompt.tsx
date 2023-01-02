ee872a734bacebcde95618c577869be314a5123c
curvesy <noobesang@gmail.com>
2023-01-02 10:21:36 -0500

    fix content beside button

--- app/components/Player/LoginPrompt.tsx
+++ app/components/Player/LoginPrompt.tsx
@@ -5,29 +5,38 @@ import {useNavigate} from 'react-router-dom';
 import {PrimaryButton} from '~/components/common/Button';
 
 const LoginPrompt = ({prop}: any) => {
-  const navigate = useNavigate();
+   const navigate = useNavigate();
 
-  const goTo = () => {
-    if (prop === 'login') {
-      navigate('/login');
-    } else if (prop === 'subscribe') {
-      navigate('/subscription');
-    }
-  };
+   const goTo = () => {
+      if (prop === 'login') {
+         navigate('/login');
+      } else if (prop === 'subscribe') {
+         navigate('/subscription');
+      }
+   };
 
-  return (
-    <>
-      <section className="playingMusic">
-        <header>
-          <h5>Login before you continue</h5>
-          <span>Or sign up and get 2 month free</span>
-        </header>
-      </section>
-      <PrimaryButton className="loginButton" theme="white" onClick={goTo}>
-        {prop}
-      </PrimaryButton>
-    </>
-  );
+   return (
+      <>
+         <section className="playingMusic">
+            <header>
+               {prop === 'login' ? (
+                  <>
+                     <h5>login before you continue</h5>
+                     <span>Or sign up and get 2 month free</span>
+                  </>
+               ) : (
+                  <>
+                     <h5>you need subscription</h5>
+                     <span>to continue listening to music</span>
+                  </>
+               )}
+            </header>
+         </section>
+         <PrimaryButton className="loginButton" theme="white" onClick={goTo}>
+            {prop}
+         </PrimaryButton>
+      </>
+   );
 };
 
 export default LoginPrompt;
