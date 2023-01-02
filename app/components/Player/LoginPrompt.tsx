d4dde78c6f16447ab76674b4381b2813a48b5ed3
curvesy <noobesang@gmail.com>
2023-01-02 09:40:43 -0500

    add subscription state to loginPromp button

--- app/components/Player/LoginPrompt.tsx
+++ app/components/Player/LoginPrompt.tsx
@@ -1,34 +1,39 @@
 /* External dependencies */
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 /* Internal dependencies */
-import { PrimaryButton } from '~/components/common/Button';
+import {PrimaryButton} from '~/components/common/Button';
+import {useAccount} from '~/hooks/useAccount/useAccount';
+import {useActiveSubscription} from '~/hooks/useSubscriptions';
+// import { SubscriptionStatus } from '../../';
 
-const LoginPrompt = () => {
-  const navigate = useNavigate();
+type Props = {};
 
-  const goToLogin = () => {
-    navigate('/login');
-  };
+const LoginPrompt = ({prop}: any) => {
+   const navigate = useNavigate();
 
-  return (
-    <>
-      <section className="playingMusic">
-        <header>
-          <h5>Login before you continue</h5>
-          <span>Or sign up and get 2 month free</span>
-        </header>
-      </section>
-      <PrimaryButton
-        className='loginButton'
-        theme="white"
-        onClick={goToLogin}
-      >
-        Login
-      </PrimaryButton>
-    </>
-  );
-}
+   const goTo = () => {
+      if (prop === 'login') {
+         navigate('/login');
+      } else if (prop === 'subscribe') {
+         navigate('/subscription');
+      }
+   };
+
+   return (
+      <>
+         <section className="playingMusic">
+            <header>
+               <h5>Login before you continue</h5>
+               <span>Or sign up and get 2 month free</span>
+            </header>
+         </section>
+         <PrimaryButton className="loginButton" theme="white" onClick={goTo}>
+            {prop}
+         </PrimaryButton>
+      </>
+   );
+};
 
 export default LoginPrompt;
--- app/components/Player/index.tsx
+++ app/components/Player/index.tsx
@@ -1,30 +1,44 @@
 /* External dependencies */
-import React, { useContext } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, {useContext} from 'react';
+import {useLocation} from 'react-router-dom';
 
 /* Internal dependencies */
-import { PlayerContext } from '~/context/playerContext/playerContextProvider';
-import { ProfileContext } from '~/context/profileContext/profileContextProvider';
+import {PlayerContext} from '~/context/playerContext/playerContextProvider';
+import {ProfileContext} from '~/context/profileContext/profileContextProvider';
 import Modal from '~/components/Modal';
 import PlayerContent from './PlayerContent';
 import LoginPrompt from './LoginPrompt';
+import {useAccount} from '~/hooks/useAccount/useAccount';
+import {useActiveSubscription} from '~/hooks/useSubscriptions';
 
 const Player = () => {
-  const { current } = useContext(PlayerContext);
-  const location = useLocation();
-  const { info } = useContext(ProfileContext);
+   const {current} = useContext(PlayerContext);
+   const location = useLocation();
 
-  const isAuthPath = ['/registered', '/login'].includes(location.pathname);
+   const {info} = useAccount();
 
-  return (
-    <Modal className={`component player ${current && !isAuthPath ? 'visible' : ''}`}>
-      {
-        info.address
-          ? <PlayerContent />
-          : <LoginPrompt />
-      }
-    </Modal>
-  );
-}
+   const {subscriptionStatus} = useActiveSubscription();
+
+   const isSubscribe = subscriptionStatus === 'SUBSCRIBED';
+   const isLogin = !!info.address;
+
+   const isAuthPath = ['/registered', '/login'].includes(location.pathname);
+
+   return (
+      <Modal
+         className={`component player ${
+            current && !isAuthPath ? 'visible' : ''
+         }`}
+      >
+         {info.address ? (
+            <PlayerContent />
+         ) : (
+            <LoginPrompt
+               prop={isLogin && !isSubscribe ? 'subscribe' : 'login'}
+            />
+         )}
+      </Modal>
+   );
+};
 
 export default Player;
