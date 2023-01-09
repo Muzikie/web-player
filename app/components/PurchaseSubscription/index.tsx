58255ce5b137227ad0df307c23d21f8029ecb1bb
Ailin <aylin.mrLarki@gmail.com>
2023-01-09 16:50:50 +0100

    Fix some bugs

--- app/components/PurchaseSubscription/index.tsx
+++ app/components/PurchaseSubscription/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, from 'react';
 import { useSubscriptions } from '~/hooks/useSubscriptions';
 import Modal from '~/components/Modal';
 import Icon from '~/components/common/Icon';
@@ -6,77 +6,72 @@ import { PrimaryButton } from '~/components/common/Button';
 import { TextLink } from '~/components/common/Link';
 import './purchase.css'
 
-const PurchaseSubscription = () => {
-
-  return (
-    <>
-      <section className="component purchase">
+const PurchaseSubscription = () => (
+  <>
+    <section className="component purchase">
+      <div className="description">
+        <header className="title">
+          <figure>
+            <img src="/images/letter.svg" alt="Letter icon" />
+          </figure>
+          <h4>
+            Share the word of Muzikie and receive free sbuscription
+          </h4>
+        </header>
+        <p>
+          Muzikie has embarked on an exciting mission to bring transparency and fairness to the music distribution industry.
+          We are excited to build it, and we need your support and feedback on this mission.
+        </p>
+      </div>
+    </section>
+    <Modal className='component offer' theme="light" notStickyInMobile>
+      <aside className="sidebar">
+        <span>Free 6K</span>
+        <span>6000 audio streams</span>
+        <span>To our new users</span>
+        <PrimaryButton className="subscribeButton">Subscribe now</PrimaryButton>
+      </aside>
+      <main className="mainOffer">
         <div className="description">
-          <div className="title">
-            <figure>
-              <img src="/images/letter.svg" alt="SVG as an image" />
-            </figure>
-            <h4>
-              Share the word of Muzikie and receive free sbuscription
-            </h4>
-          </div>
-          <p>
-            Muzikie has embarked on an exciting mission to bring transparency and fairness to the music distribution industry.
-            We are excited to build it, and we need your support and feedback on this mission.
-          </p>
+          Looking for music streaming platform where music is not treated as a commodity?
+          Support your favorite musicians by joining a fair,
+          transparent and intuitive Platform with a futuristic economy.
         </div>
-      </section>
-      <Modal className='component offer' theme="light" notStickyInMobile>
-        <div className="sidebar">
-          <span>Free 6K</span>
-          <span>6000 audio streams</span>
-          <span>To our new users</span>
-          <PrimaryButton className={`subscribeButton ${'disabled'}`}>Subscribe now</PrimaryButton>
+        <div className="social">
+          <ul>
+            <li>
+              <img src="/images/checkmark.svg" alt="checkmark icon" />
+              <TextLink
+                className="checked"
+                theme="dark"
+                title="Follow Muzikie on Twitter"
+                to="/twitter"
+              />
+            </li>
+            <li>
+              <Icon name='twitter1' />
+              <TextLink
+                className="notChecked"
+                theme="dark"
+                title="Share a tweet about Muzikie"
+                to="/twitter"
+              />
+            </li>
+            <li>
+              <Icon name='telegram' />
+              <TextLink
+                className="notChecked"
+                theme="dark"
+                title="Join the Telegram channel"
+                to="/telegram"
+              />
+            </li>
+          </ul>
         </div>
-        <div className="mainOffer">
-          <div className="description">
-            Looking for music streaming platform where music is not treated as a commodity?
-            Support your favorite musicians by joining a fair,
-            transparent and intuitive Platform with a futuristic economy.
-          </div>
-          <div className="social">
-            <ul>
-              <li>
-                <img src="/images/checkmark.svg" alt="SVG as an image" />
-                <TextLink
-                  className="checked"
-                  theme="dark"
-                  title="Follow Muzikie on Twitter"
-                  to="/twitter"
-                />
-
-              </li>
-              <li>
-                <Icon name='twitter1' />
-                <TextLink
-                  className="notChecked"
-                  theme="dark"
-                  title="Share a tweet about Muzikie"
-                  to="/twitter"
-                />
-              </li>
-              <li>
-                <Icon name='telegram' />
-                <TextLink
-                  className="notChecked"
-                  theme="dark"
-                  title="Join the Telegram channel"
-                  to="/telegram"
-                />
-
-              </li>
-            </ul>
-          </div>
-        </div>
-      </ Modal>
-    </>
-  )
-};
+      </main>
+    </ Modal>
+  </>
+);
 
 export default PurchaseSubscription;
 
