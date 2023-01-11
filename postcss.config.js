ab099b46855238b04966aeae8c2caf61ba4a18b8
Ailin <aylin.mrLarki@gmail.com>
2023-01-11 23:03:47 +0100

    Replace postcss-partial-import with postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,8 @@
-const postcssPartialImport = require('postcss-partial-import');
+const postcssImport = require('postcss-import');
 const postcssNesting = require('postcss-nesting');
 const autoprefixer = require('autoprefixer');
+const postcssImportGlob = require('postcss-import-ext-glob')
 
 module.exports = {
-  plugins: [
-    autoprefixer({}),
-    postcssPartialImport({}),
-    postcssNesting({}),
-  ],
+  plugins: [autoprefixer({}), postcssImportGlob({}), postcssImport({}), postcssNesting({})],
 };
