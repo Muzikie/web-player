b252ec8c917a8ab1dc87cbc1c74a222f1013ec61
curvesy <noobesang@gmail.com>
2023-01-26 10:21:21 -0500

    Fix css alighment of search result for audio

--- app/components/Search/SearchResult.tsx
+++ app/components/Search/SearchResult.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 import { entityThemes, Entity } from '~/components/Entity/types';
 import List from '../List';
+import { liskThemes } from '../List/types';
 import { SearchResultProps } from './types';
 
 const SearchResult = ({ results }: SearchResultProps) => {
   return (
     <section className="results">
-      {
-        Object.entries(results)
-          .filter(([, items]) => items.length)
-          .map(([key, items]) => (
-            <List
-              title={key}
-              key={key}
-              itemTheme={entityThemes.minimal}
-              items={items as Entity[]}
-            />
-          ))
-      }
+      {Object.entries(results)
+        .filter(([, items]) => items.length)
+        .map(([key, items]) => (
+          <List
+            title={key}
+            key={key}
+            theme={liskThemes.wide}
+            itemTheme={entityThemes.minimal}
+            items={items as Entity[]}
+          />
+        ))}
     </section>
   );
 };
