907c1bf105b6b8a72fdd5bbd3329f0e50e31a36f
Ali Haghighatkhah <ali@lightcurve.io>
2023-01-27 08:38:27 +0100

    Remove unused logic from the search screen

--- app/routes/__main/search/index.tsx
+++ app/routes/__main/search/index.tsx
@@ -1,38 +1,17 @@
 /* External dependencies */
-import React, { useState, ChangeEvent } from 'react';
-import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import React from 'react';
 
 /* Internal dependencies */
-import { SearchLoaderData } from '../../types';
-import { search } from '~/models/entity.server';
-import List from '~/components/List';
-import { Input } from '~/components/common/Input';
-import { Entity, entityThemes } from '~/components/Entity/types';
 import styles from '~/css/routes/__main/search.css';
 import Search from '~/components/Search';
 
 export function links() {
   return [{ rel: 'stylesheet', href: styles }];
 }
-
-export const loader = async () => {
-  return json<SearchLoaderData>({
-    result: await search('query'),
-  });
-};
-
-const SearchScreen = () => {
-  const [query, setQuery] = useState('');
-
-  // @todo implement debounce
-  const { result } = useLoaderData() as SearchLoaderData;
-
-  return (
-    <section className="screen search">
-      <Search />
-    </section>
-  );
-};
+const SearchScreen = () => (
+  <section className="screen search">
+    <Search />
+  </section>
+);
 
 export default SearchScreen;
