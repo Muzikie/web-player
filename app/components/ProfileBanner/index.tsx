43ebaf0716ca929700c60faa5181e0db87f54c5b
curvesy <noobesang@gmail.com>
2023-02-11 06:39:45 -0500

    Create and import common/Image component to profileBanner and edit profileBanner

--- app/components/ProfileBanner/index.tsx
+++ app/components/ProfileBanner/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { API_URLS, FILES } from '~/configs'
+import Image from '../common/Image'
 import { ProfileAddress } from './types'
 
 const ProfileBanner = ({data} : ProfileAddress) => {
   const {creatorAddress} = data
+
   return (
-    <section className="component profileSummary">
-    <figure className="photo">
-      <img src={`${API_URLS.STREAMER}/${creatorAddress}-${FILES.profile}.jpg`} alt={creatorAddress}/>
-    </figure>
-    <header>
-      <h1>{ creatorAddress }</h1>
-    </header>
-  </section>
+    <section className="component profileBanner">
+      <header>
+        <figure className="bannerWrapper">
+          <Image 
+            className="photo"
+            src={`${API_URLS.STREAMER}/${creatorAddress}-${FILES.profile}.jpg`}
+            placeHolder="/images/artist.jpg"
+            alt={creatorAddress}/> 
+        </figure>
+        <h1>{ creatorAddress }</h1>
+      </header>
+    </section>
   );
-};
+  }
 
 export default ProfileBanner;
---
+++ app/components/common/Image/index.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from 'react';
+import { ImageType } from './types'
+
+const Image = ({src  , alt , className, placeHolder} : ImageType) => {
+  const image = useRef<HTMLImageElement>(null);
+
+  const imageNotFound = () => {
+    if (image.current) {
+      image.current.src = placeHolder;
+    }
+  };
+
+  useEffect(() => {
+    if (image.current?.naturalWidth === 0) {
+      imageNotFound();
+    }
+  }, [src,image.current]);
+
+  return (
+    <img ref={image} className={className} src={src} alt={alt} />
+  )
+};
+
+export default Image;
