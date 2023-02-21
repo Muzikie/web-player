704a57f48afa4c3ba985f58a4e5904f2ddb15a09
Ailin <aylin.mrLarki@gmail.com>
2023-02-21 19:58:39 +0100

    Implement WalletDetails component

--- app/components/WalletDetails/index.tsx
+++ app/components/WalletDetails/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { WalletAddressProps } from './types';
+import CopyButton from '../common/CopyButton';
 
-const WalletDetails = () => {
+const WalletDetails = ({ address }: WalletAddressProps) => {
+  //{info.balances && info.balances.length === 0 ? 0 : info.balances[0].availableBalance}
+
   return (
-    <div>
-      <h1>WalletDetails</h1>
-    </div>
+    <section className="component walletDetails">
+      <header className='walletHeader'>
+        <span className='walletAddress'>{address}</span>
+        <CopyButton text={address} />
+      </header>
+      <div className='balance'>
+        <span className='balanceTitle'>balance:</span>
+        <span className='balanceValue'>0 MZK</span>
+      </div>
+    </section>
   );
 };
 
