b2803b05ab155073381e6e19b0eb8fb3c58b7f27
curvesy <noobesang@gmail.com>
2023-02-21 09:45:46 -0500

    Add top 4 audios and empty state to popularAdios component

--- app/components/PopularAudios/index.tsx
+++ app/components/PopularAudios/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { PrimaryButton } from '../common/Button';
+import EmptyState from '../common/EmptyState';
+import EntityRow from '../Entity/EntityRow';
+import { entityThemes } from '../Entity/types';
+import { PopularAdiosProps } from './types';
 
-const PopularAudios = () => {
+const PopularAudios = ({ audios }: PopularAdiosProps) => {
   return (
-    <div>
-      <h1>PopularAudios</h1>
-    </div>
+    <section className='component popularAudios tabContainer'>
+      {audios.length === 0 ? (
+        <EmptyState
+          title='You don’t have audios or collection yet.'
+          subtitle='Start by creating your first collection, then add audios to it.'
+          content={
+            <Link to='/profile/createCollection'>
+              <PrimaryButton className='newCollectionButton'>New collection</PrimaryButton>
+            </Link>
+          }
+        />
+      ) : (
+        audios
+          .filter((_, index) => index < 4)
+          .map((audio, index) => (
+            <EntityRow
+              key={`track-${audio.audioID}-${index}`}
+              data={audio}
+              theme={entityThemes.normal}
+            />
+          ))
+      )}
+    </section>
   );
 };
 
