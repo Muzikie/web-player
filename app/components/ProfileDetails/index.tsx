e9030a6ae123a224f2a67462358b0e09ed6f9482
Ali Haghighatkhah <ali@lightcurve.io>
2023-03-05 15:18:02 +0100

    Make profile info dynamic

--- app/components/ProfileDetails/index.tsx
+++ app/components/ProfileDetails/index.tsx
@@ -3,56 +3,93 @@ import React from 'react';
 // internal
 import { PrimaryButton } from '../common/Button';
 import Icon from '../common/Icon';
-import { Link } from '../common/Link';
+import Image from '../common/Image';
+import { Profile, SocialAccountPlatform, API_URLS, FILES } from '~/configs';
+import { Placeholder, ProfileDetailsProps } from './types'
 
-const ProfileDetails = () => {
+const platforms = [
+  'instagram',
+  'twitter',
+  'youtube'
+];
+
+const merge = (data: Profile, placeholder: Placeholder) => {
+  const profile = { ...placeholder, ...data };
+  profile.socialAccounts = placeholder.socialAccounts.map(({ platform }) => ({
+    platform,
+    username: data.socialAccounts?.find(item => item.platform === platform)?.username ?? '',
+  }));
+  return profile;
+};
+
+const ProfileDetails = ({ data }: ProfileDetailsProps) => {
+  const placeholder: Placeholder = {
+    name: 'Artist Name',
+    description: 'Here artists can introduce themselves.',
+    socialAccounts: [
+      {
+        username: '',
+        platform: SocialAccountPlatform.Twitter,
+      },
+      {
+        username: '',
+        platform: SocialAccountPlatform.Instagram
+      },
+      {
+        username: '',
+        platform: SocialAccountPlatform.Youtube,
+      }
+    ],
+  };
+  const profile = merge(data, placeholder);
+
   return (
     <section className='component profileDetails'>
-      <figure>
-        <img src="/images/artist.jpg" alt='' />
+      <figure className="profileAvatar">
+        <Image
+          src={`${API_URLS.STREAMER}/${profile.creatorAddress}-${FILES.profile}.jpg`}
+          alt={profile.name}
+          placeHolder="/images/artist.jpg"
+        />
       </figure>
-      <header>
-        <h2>Artist Name</h2>
+
+      <header className="profileName">
+        <h2>{profile.name}</h2>
       </header>
+
       <section className="bioContainer">
         <h4>BIO</h4>
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, labore iusto
-          necessitatibus adipisci pariatur ratione? Aperiam possimus debitis assumenda culpa optio
-          ipsa maiores at voluptatum tenetur, nesciunt fuga praesentium aut!
+          { profile.description }
         </p>
       </section>
-      <div className="social">
+
+      <div className="socialMediaAccounts">
         <ul>
-          <li>
-            <Link theme="dark" to="/youtube">
-              <div className='iconBackground'>
-                <Icon name='youtube' />
-              </div>
-              <span>Not yet</span>
-            </Link>
-          </li>
-          <li>
-            <Link theme='dark' to="/twitter">
-              <div className="iconBackground">
-                <Icon name="twitter" />
-              </div>
-              <span>Not yet</span>
-            </Link>
-          </li>
-          <li>
-            <Link theme="dark" to="/instagram">
-              <div className="iconBackground">
-                <Icon name="instagram" />
-              </div>
-              <span>Not yet</span>
-            </Link>
-          </li>
+          {
+            profile.socialAccounts.map(({ platform, username }) => (
+              <li key={platform}>
+                <a
+                  href={username ? `https://${platforms[platform]}.com/${username}` : ''}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className='iconBox'>
+                    <Icon name={platforms[platform]} />
+                  </div>
+                  <span>{username || 'Not yet'}</span>
+                </a>
+              </li>
+            ))
+          }
         </ul>
         <PrimaryButton onClick={() => null} className="white" disabled={false} type="button">
-        Edit
+          Edit
         </PrimaryButton>
       </div>
+      <figure className="bubblesPattern">
+        <img src="/images/bubbles.svg" />
+      </figure>
     </section>
   );
 };
