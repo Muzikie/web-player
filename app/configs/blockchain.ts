73564e7b41d7375abd0a1cfa15943dee79f3d2ba
Ali Haghighatkhah <ali@lightcurve.io>
2023-03-09 17:39:26 +0100

    Prepare schemas and types for a more generic usage

--- app/configs/blockchain.ts
+++ app/configs/blockchain.ts
@@ -1,22 +1,22 @@
-export const MODULES = {
-  TOKEN: 'token',
-  AUTH: 'auth',
-  COLLECTION: 'collection',
-  AUDIO: 'audio',
-  SUBSCRIPTION: 'subscription',
-  PLAYLIST: 'playlist',
-  PROFILE: 'profile'
-};
+export enum MODULES {
+  TOKEN = 'token',
+  AUTH = 'auth',
+  COLLECTION = 'collection',
+  AUDIO = 'audio',
+  SUBSCRIPTION = 'subscription',
+  PLAYLIST = 'playlist',
+  PROFILE = 'profile',
+}
 
-export const COMMANDS = {
-  STREAM: 'stream',
-  CREATE: 'create',
-  PURCHASE: 'purchase',
-  TRANSFORM: 'transform',
-  DESTROY: 'destroy',
-  SET_ATTRIBUTES: 'setAttributes',
-  UPDATE_MEMBERS: 'updateMembers',
-};
+export enum COMMANDS {
+  STREAM = 'stream',
+  CREATE = 'create',
+  PURCHASE = 'purchase',
+  TRANSFORM = 'transform',
+  DESTROY = 'destroy',
+  SET_ATTRIBUTES = 'setAttributes',
+  UPDATE_MEMBERS = 'updateMembers',
+}
 
 export const FILES = {
   [MODULES.AUDIO]: 'cover',
@@ -24,4 +24,3 @@ export const FILES = {
   [MODULES.PROFILE]: 'avatar',
   [MODULES.PLAYLIST]: 'cover',
 };
-
