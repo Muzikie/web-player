ffe52a2dc46c654f2e64e75e395945b51ca2f381
curvesy <noobesang@gmail.com>
2023-03-27 15:02:11 -0400

    Add utils test with error to fix it by colabrators

--- app/hooks/useBroadcast/utils.test.ts
+++ app/hooks/useBroadcast/utils.test.ts
@@ -1,17 +1,76 @@
-// import { signTransaction } from './utils';
+import { cryptography } from "@liskhq/lisk-client"
+import { accountMock } from "../../../test/mocks/account"
+import { COMMANDS, MODULES, SCHEMAS, VALID_GENRES } from "~/configs"
+import { bufferize } from "~/helpers/convertors"
+import { signTransaction } from "./utils"
 
-describe('signTransaction', () => {
-  it.todo('should return error if the schema was not found');
-  it.todo('should return error if transaction does not match the schema');
-  describe('Valid transactions', () => {
-    // signature is a buffer of length 64. check if all expected values are returned
-    it.todo('should sign audio/create transaction given valid props');
-    it.todo('should sign audio/stream transaction given valid props');
-    it.todo('should sign collection/create transaction given valid props');
-    it.todo('should sign collection/transfer transaction given valid props');
-    it.todo('should sign collection/setAttributes transaction given valid props');
-    it.todo('should sign profile/create transaction given valid props');
-    it.todo('should sign profile/setAttributes transaction given valid props');
-    it.todo('should sign subscription/purchase transaction given valid props');
+
+
+
+describe('useBroadcast', () => {
+  const account = {
+    publicKey: accountMock.publickey,
+    privateKey: accountMock.privateKey,
+    // address: cryptography.address.getAddressFromLisk32Address(accountMock.address),
+    address: accountMock.address,
+    balances: [],
+    nonce: '1',
+  };
+
+  const name = 'Test Audio';
+
+  const fileContent = new Uint8Array([0x01, 0x02, 0x03, 0x04, 0x05, 0x06]);
+  const mockFile = {
+    name: 'filename.mp3',
+    type: 'audio/mpeg',
+    lastModified: Date.now(),
+    size: fileContent.length,
+    arrayBuffer: () => Promise.resolve(fileContent.buffer),
+  } as File;
+
+ 
+  it('should sign audio/create transaction given valid props', async () => {
+    const command = COMMANDS.CREATE;
+    const module = MODULES.AUDIO;
+    const params = {
+      name,
+      releaseYear: "1994",
+      fit: [],
+      genre: [VALID_GENRES[0].value],
+      collectionID: bufferize('ef2f53fca6966b5f5f6673f6ef3b0c17'),
+      owners: [
+        {
+          address:  cryptography.address.getAddressFromLisk32Address('lskhqy429nwm2tew3j5j29ef6pguyynf6jxcmgrh2'),
+          shares: 100,
+        },
+      ],
+    };
+
+
+
+    const files: { value: File; key: string }[] = [  { value: mockFile, key: 'audio' }];
+
+   
+    
+    let result = await signTransaction({ command, module, params, files, account });
+ 
+    expect(result).toMatchObject({
+      txId: expect.any(String),
+      txBytes: expect.any(Buffer),
+      transaction: expect.any(Object),
+    });
   });
 });
+  
+  // describe('Valid transactions', () => {
+  // it.todo('should return error if transaction does not match the schema');
+//   // signature is a buffer of length 64. check if all expected values are returned
+//   it.todo('should sign audio/create transaction given valid props');
+//   it.todo('should sign audio/stream transaction given valid props');
+//   it.todo('should sign collection/create transaction given valid props');
+//   it.todo('should sign collection/transfer transaction given valid props');
+//   it.todo('should sign collection/setAttributes transaction given valid props');
+//   it.todo('should sign profile/create transaction given valid props');
+//   it.todo('should sign profile/setAttributes transaction given valid props');
+//   it.todo('should sign subscription/purchase transaction given valid props');
+// });
---
+++ test/mocks/account.ts
@@ -0,0 +1,7 @@
+export const accountMock = {
+  passphrase: 'avoid good plug behave special pull attend eagle crack fan sibling retire',
+  privateKey:
+    '9cddd7cecd93d254b830bceeafe799066a671c2ec3e91813df441d9b7863fdbb86801e49a659c05d72d005fa8bdcb10ebb9654193815b337fe02cbe0b3defab3',
+  publickey: '86801e49a659c05d72d005fa8bdcb10ebb9654193815b337fe02cbe0b3defab3',
+  address: 'lsk6cpsc8vf93bpnzrb4a2ekt72g2cprj3kpbsu2g',
+};
