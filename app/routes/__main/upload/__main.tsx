33d79e59dbb9a331553c5dced355468d0303a83a
Ailin <aylin.mrLarki@gmail.com>
2023-04-17 15:51:21 +0200

    Get address from session in loader of upload page and add WalletDetails component to upload page

--- app/routes/__main/upload/__main.tsx
+++ app/routes/__main/upload/__main.tsx
@@ -1,27 +1,49 @@
 /* External dependencies */
 import React from 'react';
 import { Outlet } from '@remix-run/react';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 /* Internal dependencies */
 import styles from '~/css/routes/__main/upload.css';
 import { ROUTES } from '~/routes/routes';
 import { Tabs } from '~/components/common/Tabs';
+import WalletDetails from '~/components/WalletDetails';
+import { uploadLoaderProps, UploadLoaderData } from '../../types';
+import { getSession } from '~/hooks/useSession';
 
 export function links() {
   return [{ rel: 'stylesheet', href: styles }];
 }
 
-const UploadScreen = () => (
-  <section className="screen upload">
-    <header className='tabsHeader'>
-      <Tabs
-        items={[
-          { title: 'Add an audio', to: ROUTES.UPLOAD_AUDIO },
-          { title: 'Add a collection', to: ROUTES.UPLOAD_COLLECTION },
-        ]}
-      />
-    </header>
-    <Outlet />
-  </section>
-);
+export const loader = async ({ request }: uploadLoaderProps) => {
+  const session = await getSession(
+    request.headers.get('Cookie')
+  );
+
+  return json<UploadLoaderData>({
+    id: session.get('address')
+  });
+}
+
+const UploadScreen = () => {
+  const { id } = useLoaderData() as UploadLoaderData;
+
+  return (
+    <section className="screen upload">
+      <WalletDetails address={id} />
+      <div className='tab'>
+        <header className='tabsHeader'>
+          <Tabs
+            items={[
+              { title: 'Add an audio', to: ROUTES.UPLOAD_AUDIO },
+              { title: 'Add a collection', to: ROUTES.UPLOAD_COLLECTION },
+            ]}
+          />
+        </header>
+        <Outlet />
+      </div>
+    </section>
+  );
+}
 
 export default UploadScreen;
