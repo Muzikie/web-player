e0f9f225f3f6c5fae0e981803f92c2e2b997a625
reyraa <ali@muzikie.com>
2023-07-11 16:43:17 +0200

    Fix the use of React form register

--- app/components/common/Select/index.tsx
+++ app/components/common/Select/index.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { UseFormRegister } from 'react-hook-form';
+import React, { forwardRef } from 'react';
 
 import { SelectProps } from './types';
 
-export const Select = <T extends string | number,>({
+const Select = <T extends string | number,>({
   options,
-  register,
-  name,
   placeholder,
-}: SelectProps<T> & { register: UseFormRegister<any> }) => (
-    <section className={`component select ${register(name)  ? '' : 'notSelected'}`}>
-      <select {...register(name)} >
-        {
-          placeholder && <option value="">{placeholder}</option>
-        }
-        {options.map((option) => {
-          const val = typeof option === 'string' || typeof option === 'number' ? option : option.value;
-          return (
-            <option key={val} value={val}>
-              {typeof option === 'string' || typeof option === 'number' ? option : option.label}
-            </option>
-          );
-        })}
-      </select>
-    </section>
-  );
+  ...restProps
+}: SelectProps<T>, ref) => (
+  <section className={`component select ${restProps.name  ? '' : 'notSelected'}`}>
+    <select {...restProps} ref={ref}>
+      {
+        placeholder && <option value="">{placeholder}</option>
+      }
+      {options.map((option) => {
+        const val = typeof option === 'string' || typeof option === 'number' ? option : option.value;
+        return (
+          <option key={val} value={val}>
+            {typeof option === 'string' || typeof option === 'number' ? option : option.label}
+          </option>
+        );
+      })}
+    </select>
+  </section>
+);
+
+export default forwardRef(Select);
