305ce42e116c901f7e901b8c2e85f2baabf55e5c
reyraa <ali@muzikie.com>
2023-07-27 10:29:40 +0200

    Improve file input feedback and placeholder

--- app/components/common/Input/File.tsx
+++ app/components/common/Input/File.tsx
@@ -1,5 +1,5 @@
 /* External dependencies */
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState, ChangeEvent } from 'react';
 
 /* Internal dependencies */
 import Icon from '../Icon';
@@ -11,23 +11,36 @@ const FileInput = ({
   multiple = true,
   className = '',
   icon = 'file',
+  onChange,
   ...restProps
-}: FileInputProps, ref) => (
-  <label className={`component fileInput ${className}`}>
-    {
-      icon && (
-        <Icon name={restProps.name ? 'check' : icon} />
-      )
-    }
-    <input
-      type="file"
-      accept={accept}
-      multiple={multiple}
-      {...restProps}
-      ref={ref}
-    />
-    <span>{placeholder}</span>
-  </label>
-);
+}: FileInputProps, ref) => {
+  console.log(restProps);
+  const [fileName, setFileName] = useState('');
 
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFileName(e.target.files && e.target.files.length ? e.target.files[0].name : '');
+    if (typeof onChange === 'function') {
+      onChange(e);
+    }
+  };
+
+  return (
+    <label className={`component fileInput ${className}`}>
+      {
+        icon && (
+          <Icon name={fileName ? 'check' : icon} />
+        )
+      }
+      <input
+        type="file"
+        accept={accept}
+        multiple={multiple}
+        onChange={handleFileChange}
+        {...restProps}
+        ref={ref}
+      />
+      <span>{fileName || `${placeholder} (${accept})`}</span>
+    </label>
+  );
+}
 export default forwardRef(FileInput);
